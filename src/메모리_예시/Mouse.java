10ce34c7b7c130edf7ba31297ed786316324fde4
saul <dlathf3210@naver.com>
2023-07-09 17:35:29 +0900

    0709

--- src/메모리_예시/Mouse.java
+++ src/메모리_예시/Mouse.java
@@ -4,6 +4,14 @@ public class Mouse {
 
     public String name;
 
+    public int age;
+
+    public int countOfTail;
+
+    public void sing(){
+        System.out.println(name + " 찍찍");
+    }
+
 
 
 }
---
+++ src/메모리_예시/MouseDriver.java
@@ -0,0 +1,24 @@
+package 메모리_예시;
+
+public class MouseDriver {
+    public static void main(String[] args) {
+
+        Mouse mickey = new Mouse();
+        mickey.name = "미키";
+        mickey.age = 85;
+        mickey.countOfTail = 1;
+        mickey.sing();
+
+        mickey = null;
+
+        Mouse jerry = new Mouse();
+        jerry.name = "제리";
+        jerry.age = 73;
+        jerry.countOfTail = 1;
+
+        jerry.sing();
+
+
+
+    }
+}
