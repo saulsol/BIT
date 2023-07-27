2af386dcc54e118815f119f0a71d3ccad1ed5684
saul <dlathf3210@naver.com>
2023-07-27 13:10:56 +0900

    0727

--- src/designPattern/proxy/RealImage.java
+++ src/designPattern/proxy/RealImage.java
@@ -1,4 +1,22 @@
 package designPattern.proxy;
 
-public class RealImage {
+public class RealImage implements Image {
+
+    private String fileName;
+
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    private void loadFromDisk(String fileName){
+
+        System.out.println("Loading : " + fileName);
+    }
+
+    @Override
+    public void displayImage() {
+        System.out.println("Displaying " + fileName);
+    }
 }
